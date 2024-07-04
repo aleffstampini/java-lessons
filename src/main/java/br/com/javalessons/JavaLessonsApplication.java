536d34d4fdab78e67a7db8710e011fb0b1afc461
aleffstampini <aleffkelvin16@gmail.com>
2024-07-03 21:46:55 -0300

    build: create structure project

---
+++ src/main/java/br/com/javalessons/JavaLessonsApplication.java
@@ -0,0 +1,13 @@
+package br.com.javalessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLessonsApplication.class, args);
+	}
+
+}
